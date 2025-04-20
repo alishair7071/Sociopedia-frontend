621ce358345f4b0be21c3e2240a6a0f1ad18acb2
Ali_Shair <alishair7071@gmail.com>
2025-04-20 10:43:19 +0500

    avoid from adding itself as a friend

--- src/components/Friend.jsx
+++ src/components/Friend.jsx
@@ -1,14 +1,13 @@
 import { useTheme, Typography, IconButton, Box } from "@mui/material";
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
-import  FlexBetween  from "./FlexBetween.jsx";
-import  UserImage  from "./UserImage.jsx";
+import FlexBetween from "./FlexBetween.jsx";
+import UserImage from "./UserImage.jsx";
 import { PersonRemoveOutlined, PersonAddOutlined } from "@mui/icons-material";
 import { setFriends } from "../state/index.js";
 
 const Friend = ({ friendId, name, subtitle, imageUrl }) => {
-
-  console.log("Friend is called url of image: "+imageUrl);
+  console.log("Friend is called url of image: " + imageUrl);
 
   const { palette } = useTheme();
   const dispatch = useDispatch();
@@ -36,37 +35,37 @@ const Friend = ({ friendId, name, subtitle, imageUrl }) => {
       }
     );
     const data = await response.json();
-    dispatch(setFriends({friends: data}));
-  }
-    return (
-      
-      <FlexBetween>
-        <FlexBetween gap="1rem">
-          <UserImage image={imageUrl} size="55px" />
-          <Box
-            onClick={() => {
-              navigate(`/profile/${friendId}`);
-              navigate(0);
+    dispatch(setFriends({ friends: data }));
+  };
+  return (
+    <FlexBetween>
+      <FlexBetween gap="1rem">
+        <UserImage image={imageUrl} size="55px" />
+        <Box
+          onClick={() => {
+            navigate(`/profile/${friendId}`);
+            navigate(0);
+          }}
+        >
+          <Typography
+            color={main}
+            variant="h5"
+            fontWeight="500"
+            sx={{
+              "&:hover": {
+                color: palette.primary.light,
+                cursor: "pointer",
+              },
             }}
           >
-            <Typography
-              color={main}
-              variant="h5"
-              fontWeight="500"
-              sx={{
-                "&:hover": {
-                  color: palette.primary.light,
-                  cursor: "pointer",
-                },
-              }}
-            >
-              {name}
-            </Typography>
-            <Typography color={medium} fontSize="0.75rem">
-              {subtitle}
-            </Typography>
-          </Box>
-        </FlexBetween>
+            {name}
+          </Typography>
+          <Typography color={medium} fontSize="0.75rem">
+            {subtitle}
+          </Typography>
+        </Box>
+      </FlexBetween>
+      {friendId != _id && (
         <IconButton
           onClick={() => patchFriend()}
           sx={{ backgroundColor: primaryLight, p: "0.6rem" }}
@@ -77,8 +76,9 @@ const Friend = ({ friendId, name, subtitle, imageUrl }) => {
             <PersonAddOutlined sx={{ color: primaryDark }} />
           )}
         </IconButton>
-      </FlexBetween>
-    );
-  };
+      )}
+    </FlexBetween>
+  );
+};
 
 export default Friend;
